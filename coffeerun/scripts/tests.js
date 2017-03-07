4f2ab0b3169105b27a380b9e11e4337280f6dbf0
Jennifer Kang <jennikang@csu.fullerton.edu>
2017-03-06 21:43:46 -0800

    finished

--- coffeerun/scripts/tests.js
+++ coffeerun/scripts/tests.js
@@ -1,19 +1,66 @@
-QUnit.test("DataStore", function(assert) {
-  var ds = new App.DataStore();
-  //undefined
-  ds.add('m@bond.com', 'tea');
-  //undefined
-  ds.add('james@bond.com', 'eshpressho');
-  //undefined
+/* global QUnit: false, App: false, myTruck: true */
 
-  var expected = object {james@bond.com: "eshpressho", m@bond.com: "tea"};
-  var actual = object {james@bond.com: "eshpressho", m@bond.com: "tea"};
-  assert.deepEqual(actual, expected, "Items are deeply equal");
+//This is for the 8.10 example
+QUnit.test('Create DataStore', function(assert) {
+    var ds = new App.DataStore();
+    ds = new App.DataStore();
+    ds.add('m@bond.com', 'tea');
+    ds.add('james@bond.com', 'eshpressho');
+    assert.deepEqual(ds.getAll(), {
+        'm@bond.com': 'tea',
+        'james@bond.com': 'eshpressho'
+    }, 'All the stuff!');
+    ds.remove('james@bond.com');
+    assert.deepEqual(ds.getAll(), {
+        'm@bond.com': 'tea'
+    }, 'All the stuff!');
+    assert.equal(ds.get('m@bond.com'), 'tea');
+    ds.get('james@bond.com');
 });
+//Eslint problems:
+// - QUnit is not defined
+// - App is not defined (x2)
 
-  //Object {james@bond.com: "eshpressho", m@bond.com: "tea"}
 
-/*ds.remove('james@bond.com');
-ds.getAll();
-ds.get('m@bond.com');
-ds.get('james@bond.com');*/
+//This is for the 8.32 example
+QUnit.test('truck', function(assert) {
+
+    //var myTruck = new Truck('ncc-1701', new DataStore());
+    myTruck.createOrder({
+        emailAddress: 'me@goldfinger.com',
+        coffee: 'double mocha'
+    });
+    myTruck.createOrder({
+        emailAddress: 'dr@no.com',
+        coffee: 'decaf'
+    });
+    myTruck.createOrder({
+        emailAddress: 'm@bond.com',
+        coffee: 'earl grey'
+    });
+    myTruck.printOrders();
+    assert.deepEqual(myTruck.getAllTruck(), {
+        'me@goldfinger.com': {
+            'coffee': 'double mocha',
+            'emailAddress': 'me@goldfinger.com'
+        }
+        'dr@no.com': {
+            'coffee': 'decaf',
+            'emailAddress': 'dr@no.com'
+        },
+        'm@bond.com': {
+            'coffee': 'earl grey',
+            'emailAddress': 'm@bond.com'
+        }
+    });
+    myTruck.deliverOrder('dr@no.com');
+    myTruck.deliverOrder('m@bond.com');
+    myTruck.printOrders();
+    assert.deepEqual(myTruck.getAllTruck(), {
+        'me@goldfinger.com': {
+            'coffee': 'double mocha',
+            'emailAddress': 'me@goldfinger.com'
+        }
+    });
+
+});
